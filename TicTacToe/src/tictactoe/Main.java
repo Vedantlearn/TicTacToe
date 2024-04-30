7265c3be6f71f628be3d8a80de72925a41327952
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-05-01 00:50:26 +0530

    Update Main.java

--- TicTacToe/src/tictactoe/Main.java
+++ TicTacToe/src/tictactoe/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 	  public static void main(String[] args) {
 	    char[][] board = new char[3][3];
-	    for (int row = 0; row < board.length; row++) {
+	    for (int row = 0; row < board.length; row++) { 
 	      for (int col = 0; col < board[row].length; col++) {
 	        board[row][col] = ' ';
 	      }
