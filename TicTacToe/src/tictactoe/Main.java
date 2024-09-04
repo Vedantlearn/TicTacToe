b68134681ceae180bd46d1913b7965acafcb8fc9
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-09-04 22:36:38 +0530

    Update Main.java

--- TicTacToe/src/tictactoe/Main.java
+++ TicTacToe/src/tictactoe/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	    char[][] board = new char[3][3];
 	    for (int row = 0; row < board.length; row++) { 
 	      for (int col = 0; col < board[row].length; col++) {      
-	        board[row][col] = ' ';    
+	        board[row][col] = ' ';      
 	      }
 	    }                          
 	    char player = 'X'; 
